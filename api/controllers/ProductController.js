92819c8b29400799abe4768f5534dde1210ad632
BERNATENE Gael <bg704531@iutnice.unice.fr>
2018-01-08 17:48:39 +0100

    second commit

--- api/controllers/ProductController.js
+++ api/controllers/ProductController.js
@@ -8,24 +8,48 @@
 module.exports = {
 	create: function(req, res) {
 
+		var rp = require('fs.realpath');
 		var dir = '../../assets/uploads/';
+		var real;
 		
+
 		req.file('avatar').upload({
     			// don't allow the total upload size to exceed ~100MB
     			maxBytes: 100000000,
     			// set the directory
     			dirname: dir
   			},function (err, uploadedFile) {
-    			// if error negotiate
    	 			if (err) return res.serverError(err);
     			// logging the filename
-    			console.log(uploadedFile);
-    			return res.json({msg: 'The tran file are transfered'});
+
+
+    			var up = uploadedFile[0].filename;
+    			var dirBase = dir.toString() + up.toString()
+
+    			real = rp.realpathSync(uploadedFile[0].fd.toString());
+
+    			Product.create({ 
+					reference: Math.random().toString(36),
+					title: req.param('title'), 
+					price: req.param('price'),
+					description: req.param('description'),
+					image: real.toString(),
+					categoryid: 1 
+				} ,function(err, created) {
+				 	if(!err) {
+	        			console.log('Produit créé : '+created.title+', ayant pour reference : '+created.reference+'.');
+	        			res.redirect('/');
+	   			 	} else {
+	       				 return err;
+	    		 	}
+
+				});
+    			//return real;	
+    			//return res.json({msg : 'no problem'});
+
   		});
 
 		
-
-
 
 		
 		/*Product.create({ 
@@ -33,7 +57,7 @@ module.exports = {
 				title: req.param('title'), 
 				price: req.param('price'),
 				description: req.param('description'),
-				image: "test",
+				image: real.toString(),
 				categoryid: 1 
 			} ,function(err, created) {
 				 if(!err) {
@@ -43,7 +67,6 @@ module.exports = {
 	       			 return err;
 	    		 }
 
-		}); */
+		});*/
 	}
 };
-
